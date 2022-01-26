5f1f14c6203d6f39633a296afbf1427c1ced0bbb
VerifyingUsername <lkyben6@gmail.com>
2022-01-26 17:24:20 +0800

    Mouse bug fixes and Coin rotation

--- Assets/Scripts/ShowCursor.cs
+++ Assets/Scripts/ShowCursor.cs
@@ -7,6 +7,7 @@ public class ShowCursor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
 
